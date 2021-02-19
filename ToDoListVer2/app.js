6d64df69b5737c6da0a056469eb57090e0493e8d
will zhang <zhanliam21@gmail.com>
2021-02-18 22:59:22 -0500

    346 custom lists; express route params

--- ToDoListVer2/app.js
+++ ToDoListVer2/app.js
@@ -43,6 +43,16 @@ const item3 = new Item ( {
 //adding to arr
 const defaultItems = [item1, item2, item3];
 
+// new express routing schema 
+
+const listSchema = { 
+  name: String,
+  items: [itemsSchema]
+};
+
+const List = mongoose.model("List", listSchema);
+
+
 
 
 app.get("/", function(req, res) {
@@ -69,9 +79,40 @@ app.get("/", function(req, res) {
     
   });
 
-  
 
 });
+
+
+//using express routing params
+app.get("/:customListName", function(req, res) { 
+  const customListName = req.params.customListName;
+
+  List.findOne({name: customListName}, function( err, foundList) {
+    if(!err) {
+      if (!foundList) {
+        //console.log("does not exist");
+        //create a new list 
+        const list = new List( {
+          name: customListName, 
+          items: defaultItems
+        });
+      
+        list.save();
+        res.redirect("/" + customListName);
+
+      } else {
+        //console.log("Exists!");
+        //show an existing list 
+
+        res.render("list", { listTitle: foundList.name, newListItems: foundList.items});
+      }
+    }
+  });
+
+  
+
+}); 
+
 
 //adding new items to list
 app.post("/", function(req, res){
@@ -89,23 +130,23 @@ app.post("/", function(req, res){
 
 });
 
-
+//deleting items off the list and TB 
 app.post("/delete", function(req, res) {
+
   const checkedItemId = req.body.checkbox; 
+
   Item.findByIdAndRemove(checkedItemId, function(err) {
     if(!err) { 
       console.log("sucessfuly deleted");
     } 
   });
+
   res.redirect('/');
 
 
 })
 
 
-app.get("/work", function(req,res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
-});
 
 app.get("/about", function(req, res){
   res.render("about");
