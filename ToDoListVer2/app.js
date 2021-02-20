f0e968e6e395f975f39d4180794aeade3356dad4
will zhang <zhanliam21@gmail.com>
2021-02-20 17:10:18 -0500

    heroku app deployment

--- ToDoListVer2/app.js
+++ ToDoListVer2/app.js
@@ -15,7 +15,7 @@ app.use(express.static("public"));
 
 
 //--- basic mongoose setup --- 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true,
+mongoose.connect("mongodb+srv://admin-will:downbad@cluster0.vs0me.mongodb.net/todolistDB?retryWrites=true&w=majority", {useNewUrlParser: true,
 useUnifiedTopology: true});
 
 //establishing the schema for a new item object 
@@ -179,6 +179,14 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
+let port = process.env.PORT;
+if (port === null || port =="") {
+  port = 3000;
+}
+
+
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
+
+
