f41ea49ccef3f84759bbc4c59f0160b76bb5c5ce
will zhang <zhanliam21@gmail.com>
2021-02-21 22:23:19 -0500

    Level 2 - Encryption

--- Secrets/app.js
+++ Secrets/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -15,10 +16,15 @@ app.use(bodyParser.urlencoded({
 mongoose.connect("mongodb://localhost:27017/userDB", 
 { useNewUrlParser: true , useUnifiedTopology: true });
 
-const userSchema = {
+//change schema into a full mongoose schema 
+const userSchema = new mongoose.Schema ({
     email: String, 
     password: String
-};
+});
+
+//this encrypts the password when we do save() and decrypts when we do find()
+const secret = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password'] });
 
 const User = new mongoose.model("User", userSchema); 
 
