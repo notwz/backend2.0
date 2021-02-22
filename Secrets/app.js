ab5a6dda4ad2238a93781afa8ad9f6bbe579fec6
will zhang <zhanliam21@gmail.com>
2021-02-21 19:21:11 -0500

    level 1 security; retrieving and checking passwords

--- Secrets/app.js
+++ Secrets/app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -10,6 +11,18 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
+mongoose.connect("mongodb://localhost:27017/userDB", 
+{ useNewUrlParser: true , useUnifiedTopology: true });
+
+const userSchema = {
+    email: String, 
+    password: String
+};
+
+const User = new mongoose.model("User", userSchema); 
+
+
 
 app.get("/", function (req, res) {
     res.render("home");
@@ -22,6 +35,37 @@ app.get("/login", function (req, res) {
 app.get("/register", function (req, res) {
     res.render("register");
 });
+
+app.post("/register", (req, res) => {
+
+    const newUser = new User( {
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save( (err) => {
+        console.log(err || res.render("secrets"));
+    });
+});
+
+
+app.post('/login', (req,res) => {
+    const username = req.body.username;
+    const password = req.body.password; 
+
+    User.findOne({ email: username}, (err, foundUser) =>{
+        if(err) {
+            console.log(err);
+        } else {
+            if( foundUser){
+                if(foundUser.password === password) {
+                    res.render('secrets');
+                }
+            }
+        }
+    })
+}
+)
 
 
 app.listen("3000", function() {
