a4232e2d3623a72f99d70e5ebb30e40d1bfb8d67
will zhang <zhanliam21@gmail.com>
2021-02-22 23:59:10 -0500

    lv 4 salting and hashing with bcrypt [DEBUGGERS POGGERS]
    omg this one sucked. i put hash as param instead of foundUser.password. fml

--- Secrets/app.js
+++ Secrets/app.js
@@ -4,7 +4,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require("mongoose");
-const md5 = require('md5'); 
+//const md5 = require('md5'); 
+const bcrypt = require("bcrypt");
+const saltRounds = 10; 
 
 const app = express();
 
@@ -49,13 +51,19 @@ app.get("/register", function (req, res) {
 
 app.post("/register", (req, res) => {
 
-    const newUser = new User( {
-        email: req.body.username,
-        password: md5(req.body.password)
-    });
-
-    newUser.save( (err) => {
-        console.log(err || res.render("secrets"));
+    //test@bhash.com 123
+    bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+        newUser.save( (err) => {
+            if(err) {
+                console.log(err);
+            } else {
+                res.render("secrets")
+        } 
+        });
     });
 });
 
@@ -63,21 +71,30 @@ app.post("/register", (req, res) => {
 app.post('/login', (req,res) => {
     const username = req.body.username;
     //turn the password they typed and hash it; use the hashed version of the password to compare with the hashed value in our Database
-    const password = md5(req.body.password);
+    const password = req.body.password;
+    
 
-    User.findOne({ email: username}, (err, foundUser) =>{
+    User.findOne({ email: username}, function(err, foundUser) {
+        
         if(err) {
+            
             console.log(err);
         } else {
+            
             if( foundUser){
-                if(foundUser.password === password) {
-                    res.render('secrets');
-                }
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+                    if(result===true) {
+                        res.render("secrets");
+                        
+                    } else {
+                        console.log(err);
+                        
+                    }
+                });                 
             }
         }
-    })
-}
-);
+    });
+});
 
 
 app.listen("3000", function() {
