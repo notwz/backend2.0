be7e34bb1ec22e25b5e0f072b02475f47f0c67c8
will zhang <zhanliam21@gmail.com>
2021-02-23 18:54:09 -0500

    Level 5 - passport, cookies, sessions

--- Secrets/app.js
+++ Secrets/app.js
@@ -5,8 +5,12 @@ const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require("mongoose");
 //const md5 = require('md5'); 
-const bcrypt = require("bcrypt");
-const saltRounds = 10; 
+/* const bcrypt = require("bcrypt");
+const saltRounds = 10;  */
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+// dont need to require passport-local; that is a dependency for localmongoose
 
 const app = express();
 
@@ -17,6 +21,17 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+app.use( session( {
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false
+
+}));
+mongoose.set('useCreateIndex', true);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://localhost:27017/userDB", 
 { useNewUrlParser: true , useUnifiedTopology: true });
 
@@ -25,6 +40,8 @@ const userSchema = new mongoose.Schema ({
     email: String, 
     password: String
 });
+
+userSchema.plugin(passportLocalMongoose);
 
 //this encrypts the password when we do save() and decrypts when we do find()
 //we want to store our const secret as an environmental variable in our .env file
@@ -35,6 +52,10 @@ userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password'] }); */
 
 const User = new mongoose.model("User", userSchema); 
 
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser()); 
 
 
 app.get("/", function (req, res) {
@@ -49,51 +70,52 @@ app.get("/register", function (req, res) {
     res.render("register");
 });
 
-app.post("/register", (req, res) => {
+app.get("/secrets", (req, res) => {
+    if(req.isAuthenticated()) {
+        res.render("secrets");
+    } else { 
+        res.render("login");
+    }
+});
 
-    //test@bhash.com 123
-    bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
-        const newUser = new User({
-            email: req.body.username,
-            password: hash
-        });
-        newUser.save( (err) => {
-            if(err) {
-                console.log(err);
-            } else {
-                res.render("secrets")
-        } 
-        });
-    });
+app.get("/logout", (req, res) => { 
+    //end user session 
+    req.logout();
+    res.redirect("/");
+})
+
+app.post("/register", (req, res) => {
+    User.register({username: req.body.username}, req.body.password, (err, user) => {
+        if(err) {
+            console.log(err);
+            res.redirect("/register");
+        } else {
+            passport.authenticate("local")(req,res, ()=> {
+                res.redirect("secrets")
+            })
+        }
+    })
+   
 });
 
 
-app.post('/login', (req,res) => {
-    const username = req.body.username;
-    //turn the password they typed and hash it; use the hashed version of the password to compare with the hashed value in our Database
-    const password = req.body.password;
-    
 
-    User.findOne({ email: username}, function(err, foundUser) {
-        
-        if(err) {
-            
-            console.log(err);
+app.post('/login', (req,res) => {
+    //test@bhash.com 123
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    })
+    req.login( user, (err)=> {
+        if(err) { 
+            console.log(err)
         } else {
-            
-            if( foundUser){
-                bcrypt.compare(password, foundUser.password, function(err, result) {
-                    if(result===true) {
-                        res.render("secrets");
-                        
-                    } else {
-                        console.log(err);
-                        
-                    }
-                });                 
-            }
+            passport.authenticate("local")(req, res, ()=> {
+                res.redirect("secrets");
+
+            });
         }
-    });
+    })
 });
 
 
